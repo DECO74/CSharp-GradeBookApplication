9fe67fd467c409817e36a27b64b515b696cefe04
DECO74 <andre.martins@contentstuff.com>
2019-08-12 15:40:04 -0300

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -20,37 +20,45 @@ namespace GradeBook.GradeBooks
             }
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.20);
-            // The following is in LINQ 
+            // The following is in LINQ Language
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if(grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if(grades[(threshold * 2) -1])
+                return 'B';
+            else if(grades[(threshold * 3) -1])
+            
+            else if(grades[(threshold * 4) -1])
+                return 'D';
+            else
+                return 'F';
 
-            // Find Highest and Lowest Grade
-            var highestGrade = double.MinValue;
-            var lowestGrade = double.MaxValue;
-            foreach(var student in Students)
-            {
-                highestGrade = Math.Max(student.AverageGrade, highestGrade);
-                lowestGrade = Math.Min(student.AverageGrade, lowestGrade);
-            }
-            var rankScale = (highestGrade - lowestGrade) * 0.20;
+            // // Find Highest and Lowest Grade
+            // var highestGrade = double.MinValue;
+            // var lowestGrade = double.MaxValue;
+            // foreach(var student in Students)
+            // {
+            //     highestGrade = Math.Max(student.AverageGrade, highestGrade);
+            //     lowestGrade = Math.Min(student.AverageGrade, lowestGrade);
+            // }
+            // var rankScale = (highestGrade - lowestGrade) * 0.20;
 
-
-            foreach(var student in Students)
-            {
-                switch(student.AverageGrade)
-                {
-                    case var d when d >= highestGrade - rankScale:
-                        return 'A';
-                    case var d when d >= highestGrade - (2 * rankScale):
-                        return 'B';
-                    case var d when d >= highestGrade - (3 * rankScale):
-                        return 'C';
-                    case var d when d >= highestGrade - (4 * rankScale):
-                        return 'D';
-                    default:
-                        return 'F';
-                }
-            }
-            return 'F';
+            // foreach(var student in Students)
+            // {
+            //     switch(student.AverageGrade)
+            //     {
+            //         case var d when d >= highestGrade - rankScale:
+            //             return 'A';
+            //         case var d when d >= highestGrade - (2 * rankScale):
+            //             return 'B';
+            //         case var d when d >= highestGrade - (3 * rankScale):
+            //             return 'C';
+            //         case var d when d >= highestGrade - (4 * rankScale):
+            //             return 'D';
+            //         default:
+            //             return 'F';
+            //     }
+            // }
         }
     }
 }
