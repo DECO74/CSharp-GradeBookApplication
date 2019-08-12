57d99da9a9c6ff3f1dd1274cde6bc0a1d8ebe11e
DECO74 <andre.martins@contentstuff.com>
2019-08-12 14:00:44 -0300

    GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,4 @@
+using System;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -7,6 +8,45 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int rankDivider = (Students.Count * 20) / 100; 
+
+            if(Students.Count <= 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            // Find Highest and Lowest Grade
+            var highestGrade = double.MinValue;
+            var lowestGrade = double.MaxValue;
+            foreach(var student in Students)
+            {
+                highestGrade = Math.Max(student.AverageGrade, highestGrade);
+                lowestGrade = Math.Min(student.AverageGrade, lowestGrade);
+            }
+            var rankScale = (highestGrade - lowestGrade) * 0.20;
+
+
+            foreach(var student in Students)
+            {
+                switch(student.AverageGrade)
+                {
+                    case var d when d >= highestGrade - rankScale:
+                        return 'A';
+                    case var d when d >= highestGrade - (2 * rankScale):
+                        return 'B';
+                    case var d when d >= highestGrade - (3 * rankScale):
+                        return 'C';
+                    case var d when d >= highestGrade - (4 * rankScale):
+                        return 'D';
+                    default:
+                        return 'F';
+                }
+            }
+            return 'F';
         }
     }
 }
